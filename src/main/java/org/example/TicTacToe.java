9ccf6f411e174f38eb070a1888ff83ec151cb240
kopernike <wi23b052@technikum-wien.at>
2024-07-07 17:41:01 +0200

    Class TicTacToe finished

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -13,4 +13,45 @@ public class TicTacToe {
         this.currentPlayer = player1;
         this.board = new Board();
     }
+    private void switchCurrentPlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
+
+    public void start() {}
+
+    private boolean hasWinner() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (board.cells[i][0] != ' ' && board.cells[i][0] == board.cells[i][1] && board.cells[i][0] == board.cells[i][2]) {
+                return true;
+            }
+        }
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (board.cells[0][j] != ' ' && board.cells[0][j] == board.cells[1][j] && board.cells[0][j] == board.cells[2][j]) {
+                return true;
+            }
+        }
+        // Check diagonals
+        if (board.cells[0][0] != ' ' && board.cells[0][0] == board.cells[1][1] && board.cells[0][0] == board.cells[2][2]) {
+            return true;
+        }
+        if (board.cells[0][2] != ' ' && board.cells[0][2] == board.cells[1][1] && board.cells[0][2] == board.cells[2][0]) {
+            return true;
+        }
+        return false;
+    }
+
+    private int readCoordinate() {
+        return Integer.parseInt(System.console().readLine()) - 1;
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.start();
+    }
 }
