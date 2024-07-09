720c4bc7043c12722c300442b6355c24f90a8d21
Pizza2807 <wi23b191@technikum-wien.at>
2024-07-09 17:34:33 +0200

    Class Board for ID2 edited

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -36,4 +36,13 @@ class Board {
         }
         return true;
     }
+    public void print() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cells[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
 }
