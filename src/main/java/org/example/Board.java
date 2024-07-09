9bc5970db7cb9c726420c874ec0bbbbddadb8023
Pizza2807 <wi23b191@technikum-wien.at>
2024-07-09 17:06:44 +0200

    Class Board edited

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,19 +1,25 @@
 package org.example;
 import java.util.Arrays;
 
-public class Board {
 
+class Board {
     char[][] cells;
 
     public Board() {
-        this.cells = new char[3][3];
+        cells = new char[3][3];
+        clear();
+    }
+
+    public void clear() {
         for (int i = 0; i < 3; i++) {
-            Arrays.fill(cells[i], ' ');
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = '-';
+            }
         }
     }
 
     public boolean isCellEmpty(int x, int y) {
-        return cells[x][y] == ' ';
+        return cells[x][y] == '-';
     }
 
     public void place(int x, int y, char marker) {
@@ -23,28 +29,11 @@ public class Board {
     public boolean isFull() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (cells[i][j] == ' ') {
+                if (cells[i][j] == '-') {
                     return false;
                 }
             }
         }
         return true;
     }
-
-    public void clear() {
-        for (int i = 0; i < 3; i++) {
-            Arrays.fill(cells[i], ' ');
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Board: " + "\n" +
-                " " + cells[0][0] + " | " + cells[0][1] + " | " + cells[0][2] + "\n" +
-                "---|---|---\n" +
-                " " + cells[1][0] + " | " + cells[1][1] + " | " + cells[1][2] + "\n" +
-                "---|---|---\n" +
-                " " + cells[2][0] + " | " + cells[2][1] + " | " + cells[2][2];
-    }
 }
-
